fe9116dceb5792dd2467890ae88fc96cf94f5bae
Koumare Siriki <ksiriki59@gmail.com>
2021-08-16 11:13:57 +0200

    premier test python

---
+++ conversion.py
@@ -0,0 +1,11 @@
+# Par ex, convertir 312 min en h, min
+# def conversion(n):
+#     h = int(n/60)
+#     minute = int(n-(60*h))
+#     print('le plus long film sur netflix fait', h, 'h', minute,'minute')
+
+# conversion(230)
+
+numbers = [34.6, -203.4, 68.3, -12.2, 44.6, 12.7]
+newlist = [x for x in numbers if x>0]
+print(newlist)
